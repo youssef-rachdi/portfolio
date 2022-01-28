8992070cfba423ac67972f39b2df245e14901e38
lenovo <lenovo@DESKTOP-6R21DPP>
2022-01-28 13:53:05 +0100

    Add project files.

---
+++ Infrastructer/Migrations/20211230222156_InitialCreate.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructer.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Address",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Street = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Number = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Address", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PortfolioItems",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false, defaultValueSql: "NEWID()"),
+                    ProjectName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PortfolioItems", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Owner",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false, defaultValueSql: "NEWID()"),
+                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Profil = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Avatar = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AddressId = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Owner", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Owner_Address_AddressId",
+                        column: x => x.AddressId,
+                        principalTable: "Address",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Owner",
+                columns: new[] { "Id", "AddressId", "Avatar", "FullName", "Profil" },
+                values: new object[] { new Guid("5b58b614-145d-4ffb-8e4d-dbc4eb563afe"), null, "avatar.jpg", "Khalid ESSAADANI", "Microsoft MVP / .NET Consultant" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Owner_AddressId",
+                table: "Owner",
+                column: "AddressId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Owner");
+
+            migrationBuilder.DropTable(
+                name: "PortfolioItems");
+
+            migrationBuilder.DropTable(
+                name: "Address");
+        }
+    }
+}
---
+++ Infrastructer/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,34 @@
+﻿using Core.Interfaces;
+using Infrastructer.Repository;
+using Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructer.UnitOfWork
+{
+    public class UnitOfWork<T> : IUnitOfWork<T> where T : class
+    {
+        private readonly DataContext _context;
+        private IGenericRepository<T> _entity;
+
+        public UnitOfWork(DataContext context)
+        {
+            _context = context;
+        }
+        public IGenericRepository<T> Entity
+        {
+            get
+            {
+                return _entity ?? (_entity = new GenericRepository<T>(_context));
+            }
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
